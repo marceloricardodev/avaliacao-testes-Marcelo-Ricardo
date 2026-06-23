6dcace341ace54b09031287eb20d67cb671f1e49
marceloricardodev <m_rycardo@hotmail.com>
2026-06-22 21:14:00 -0300

    Estrutura inicial

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        calculadora calc = new calculadora();
+
+        System.out.println("Soma: " + calc.soma(10, 5));
+        System.out.println("Divisão: " + calc.divisao(10, 2));
+    }
+}
---
+++ src/main/java/calculadora.java
@@ -0,0 +1,14 @@
+public class calculadora {
+
+    public int soma(int a, int b){
+        return a + b;
+    }
+
+    public int divisao(int a, int b){
+        if (b == 0) {
+            throw new ArithmeticException("Divisão por zero não permitida");
+        }
+        return a / b;
+
+    }
+}
